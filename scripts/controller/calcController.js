9b6032a9bb120b0e5ad599d6bab6f03d00335f9d
Lucianoal0511 <lucianoal0511@gmail.com>
2021-09-05 23:33:26 -0300

    Iniciando Calculadora

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,3 +1,40 @@
 class CalcController {
-    
+    constructor(){
+        this._displayCalcEl = document.querySelector('#display');//Seleciona
+        this._dateEl = document.querySelector('#data');
+        this._timeEl = document.querySelector('#hora');
+        //this._displayCalc = '0';//Atributos Privados
+        this._currentDate;//Atributos Privados
+        this.initialized();
+    }
+
+    initialized(){
+
+        //Selecionando
+        // let displayCalcEl = document.querySelector('#display');//Seleciona
+        // let dateEl = document.querySelector('#data');
+        // let timeEl = document.querySelector('#hora');
+
+        //Inserir dados
+        //displayCalcEl.innerHTML = 2345;//insere o valor
+        this._dateEl.innerHTML = "05/09/2021";
+        this._timeEl.innerHTML = "23:19";
+
+    }
+
+    get displayCalc(){
+        return this._displayCalcEl.innerHTML;
+    }
+
+    set displayCalc(valor){
+        this._displayCalcEl.innerHTML = valor;
+    }
+
+    get currentDate(){
+        return this._currentDate;
+    }
+
+    set currentDate(valor){
+        this._currentDate = valor;
+    }
 }
