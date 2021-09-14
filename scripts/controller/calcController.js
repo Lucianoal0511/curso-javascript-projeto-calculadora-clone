62a62202be5f7d24f2311a489c4f56e46f7b37a7
Lucianoal0511 <lucianoal0511@gmail.com>
2021-09-14 03:03:29 -0300

    Update calcController.js

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,5 +1,7 @@
 class CalcController {
     constructor(){
+        this._lastOperator = '';
+        this._lastNumber = '';
         this._operation = [];
         this._locate = 'pt-BR';
         this._displayCalcEl = document.querySelector('#display');//Seleciona
@@ -32,6 +34,9 @@ class CalcController {
 
         }, 1000);
 
+        //Atualizar Display
+        this.setLastNumberToDisplay();
+
         //Criando o setTimeout, ele vai parar o time
         // setTimeout(()=>{
 
@@ -50,10 +55,10 @@ class CalcController {
 
             this.addEventListenerAll(btn, 'click drag', e => {
                 //console.log(e);
-                console.log(btn.className.baseVal.replace('btn-', ''));
+                //console.log(btn.className.baseVal.replace('btn-', ''));
                 let textBtn = btn.className.baseVal.replace('btn-', '');
                 //Criando um método para execução do botão
-                this.execBtn();
+                this.execBtn(textBtn);
 
             });
             this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
@@ -72,6 +77,178 @@ class CalcController {
         })
     }
 
+    //Tratar a concatenação dos números
+    getLastOperation(){
+        return this._operation[this._operation.length - 1];
+    }
+
+    setLastOperation(value){
+        this._operation[this._operation.length - 1] = value;
+    }
+
+    isOperator(value){
+        //pode ser dessa forma
+        // if (['+', '-', '*', '/', '%'].indexOf(value) > -1){//Esse método vai buscar se foi digitado algum desses sinais do array
+        //     return true;
+        // }else {
+        //     return false;
+        // }
+        //Outra maneira mais simples
+        return (['+', '-', '*', '/', '%'].indexOf(value) > -1);
+    }
+
+    pushOperation(value){
+        this._operation.push(value);
+
+        if (this._operation.length > 3){
+
+            this.calc();
+            //console.log(this._operation)
+        }
+    }
+
+    //Método para o eval
+    getResult(){
+        console.log('getResult', this._operation);
+        return eval(this._operation.join(''));
+    }
+
+    calc(){
+
+        let last = '';
+
+        this._lastOperator = this.getLastItem();//Pega o último operador e guarda na mémoria que criamos
+
+        //Retirando o bug 
+        if (this._operation.length < 3){
+            let firstItem = this._operation[0];
+            this._operation = [firstItem, this._lastOperator, this._lastNumber];
+        }
+
+        if (this._operation.length > 3){//Tem que ter no mínimo três itens para fazer a operação
+            last = this._operation.pop();
+
+            this._lastNumber = this.getResult();//Como é true, ele pega a última operação
+
+        } else if (this._operation.length == 3){
+            
+            this._lastNumber = this.getResult(false);//Como é false, ele irá pegar o número
+
+        }
+
+        console.log('_lastOperator', this._lastOperator);
+        console.log('_lastNumber', this._lastNumber);
+
+        let result = this.getResult();
+
+        if (last == '%'){
+            result /= 100;
+            this._operation = [result];
+        } else {
+            this._operation = [result];
+            if (last) this._operation.push(last);
+        }
+        
+        //Atualizar Display
+        this.setLastNumberToDisplay();
+
+    }
+
+    getLastItem(isOperator = true){//Por padrão sempre trará o último operador
+
+        let lastItem;
+        //Uma maneira de fazer
+        // for (let i = this._operation.length-1; i >= 0; i--){
+        //     if (isOperator){
+        //         if (this.isOperator(this._operation[i])){
+        //             lastItem = this._operation[i];
+        //             break;
+        //         }
+        //     } else {
+        //         if (!this.isOperator(this._operation[i])){
+        //             lastItem = this._operation[i];
+        //             break;
+        //         }
+        //     }          
+        // }
+        //Outra forma
+        for (let i = this._operation.length-1; i >= 0; i--){
+            
+                if (this.isOperator(this._operation[i]) == isOperator){
+                    lastItem = this._operation[i];
+                    break;
+                }
+        }
+
+        if (!lastItem){
+            lastItem = (isOperator) ? this._lastOperator : this._lastNumber;//if ternário
+        }
+
+        return lastItem;
+    }
+
+    setLastNumberToDisplay(){
+        
+        let lastNumber = this.getLastItem(false);
+
+        //Por ter modificado o getLastItem eu posso suprir esse trecho
+        // for (let i = this._operation.length-1; i >= 0; i--){
+        //     if (!this.isOperator(this._operation[i])){
+        //         lastNumber = this._operation[i];
+        //         break;
+        //     }
+        // }
+
+        if (!lastNumber) lastNumber = 0;//Se o lastNumber for vazio, coloca zero
+
+        this.displayCalc = lastNumber;
+
+    }
+
+    addOperation(value){
+
+        //console.log('A', value,  isNaN(this.getLastOperation()));
+        if (isNaN(this.getLastOperation())){//String
+
+            if (this.isOperator(value)){//Trocar Operador
+                this.setLastOperation(value);
+            } else {
+                this.pushOperation(value);
+                //Atualizar Display
+                this.setLastNumberToDisplay();
+            }
+
+        }else {//Number
+
+            if (this.isOperator(value)){
+                this.pushOperation(value);
+            } else {
+
+                let newValue = this.getLastOperation().toString() + value.toString();//Concatenando os números
+                this.setLastOperation(parseFloat(newValue));
+                //Atualizar Display
+                this.setLastNumberToDisplay();
+
+            }     
+
+        }
+
+        //console.log(this._operation);
+        
+    }
+
+    //Tratando o Ponto (.)
+    addDot(){
+        let lastOperation = this.getLastOperation();
+        if (this.isOperator(lastOperation) || !lastOperation){
+            this.pushOperation('0.')
+        } else {
+            this.setLastOperation(lastOperation.toString() + '.');
+        }
+
+        this.setLastNumberToDisplay();
+    }
+
     //Execução do botão
     execBtn(value){
 
@@ -80,26 +257,43 @@ class CalcController {
                 this.clearAll();
                 break;
             case 'ce':
-                this.cancelEntre();
+                this.cancelEnter();
                 break;
             case 'soma':
-                this.clearAll();
+                this.addOperation('+');
                 break;
             case 'subtracao':
-                this.clearAll();
+                this.addOperation('-');
                 break;
             case 'multiplicacao':
-                this.clearAll();
+                this.addOperation('*');
                 break;
             case 'divisao':
-                this.clearAll();
+                this.addOperation('/');
                 break;
             case 'porcento':
-                this.clearAll();
+                this.addOperation('%');
                 break;
             case 'igual':
-                this.clearAll();
+                this.calc();
                 break;
+            case 'ponto':
+                this.addDot();
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+
             default:
                 this.setError();
                 break;
@@ -107,15 +301,18 @@ class CalcController {
     }
 
     clearAll(){
-
+        this._operation = [];
+        this._lastNumber = '';
+        this._lastOperator = '';
+        
+        //Atualizar Display
+        this.setLastNumberToDisplay();
     }
 
-    cancelEntre(){
-
-    }
-
-    addOperation(value){
-        this._operation.push(value);
+    cancelEnter(){
+        this._operation.pop();//Retira o último do array
+        //Atualizar Display
+        this.setLastNumberToDisplay();
     }
 
     setError(){
